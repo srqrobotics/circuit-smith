e3523c893b1dd33a8e9babbf4c11ad151f391ba7
Pasindu Vihangana <pasi1028@gmail.com>
2025-02-18 13:50:13 +0530

    Update home.tsx

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,8 +2,8 @@ import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Circuit Smith" },
+    { name: "ToolKit for DIY Circuits", content: "Welcome to Circuit Smith!" },
   ];
 }
 
