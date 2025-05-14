e2552c7b324908290a09cec5cbcbc6af35cba610
Pasindu Vihangana <pasi1028@gmail.com>
2025-05-14 13:27:55 +0530

    Create config4public.ts

---
+++ app/config/config4public.ts
@@ -0,0 +1,4 @@
+// API key for OpenAI
+// dummy API key for OpenAI for publishing
+export const API_KEY =
+  "Bearer sk-proj-aQwy2w9UOXU5aguflUJiGhMEJKIYLC3cmIwbBvf14wygaHTaojaksddiIt8EznPJB394erLigIxCAT3BhjkFyuzNBOlrEvz_tbwfQ47HniSIxXFT-9XnIopamjwbhzI4mT_ck3Rop9laopnI7wavASD81jwYY8YA";
