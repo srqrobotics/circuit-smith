a0e69531ba65265a4e5041f0dc7ae4f27b0b63eb
Pasindu Vihangana <pasi1028@gmail.com>
2025-07-07 10:23:07 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,13 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/circuit-smith/", // 👈 Required for GitHub Pages
-  build: {
-    outDir: "docs", // 👈 GitHub Pages requires /docs or / (root)
-  },
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
     },
   },
   plugins: [react(), tsconfigPaths()],
+  base: "/circuit-smith/",
   server: {
     port: 5173,
     open: true,
