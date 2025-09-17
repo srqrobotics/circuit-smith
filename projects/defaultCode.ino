6355adff5377b5a0bda93e48285d75dd8c0a467e
Pasindu Vihangana <pasi1028@gmail.com>
2025-09-17 16:53:25 +0530

    Updates

--- projects/defaultCode.ino
+++ projects/defaultCode.ino
@@ -1,11 +1,15 @@
 [
-  "#include <Arduino.h>",
-  "",
-  "void setup() {",
-  "    // setup code here",
-  "}",
-  "",
-  "void loop() {",
-  "    // loop code here",
+  " "
+  " "
+  "#include <Arduino.h>"
+  ""
+  "void setup() {"
+  "    // setup code here"
   "}"
+  " "
+  "void loop() {"
+  "    // loop code here"
+  "}"
+  " "
+  "",
 ]
